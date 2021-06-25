82dff278767b192f49c62422cd51303915513dcc
Theodor Truffer <tt@studer-raimann.ch>
2021-06-25 09:14:12 +0200

    plugin version & dropped 5.3 support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = "xats";
-$version = "1.0.0";
-$ilias_min_version = "5.3.0";
+$version = "1.1.0";
+$ilias_min_version = "5.4.0";
 $ilias_max_version = "5.4.999";
 $responsible = "studer + raimann ag";
 $responsible_mail = "info@studer-raimann.ch";
